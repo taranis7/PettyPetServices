9c5cbecbfecfdcb562ebd945d903b451c9c311cd
Pinar Olguc <Pinar.Olguc@monitise.com>
2017-04-15 21:59:59 +0300

    Add animal service

--- src/main/java/com/pettypet/AnimalController/AnimalController.java
+++ src/main/java/com/pettypet/AnimalController/AnimalController.java
@@ -1,23 +1,36 @@
 package com.pettypet.AnimalController;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.pettypet.model.Animal;
+import com.pettypet.service.AnimalService;
+
 @Controller
 public class AnimalController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-
+    
+    @Autowired
+    private AnimalService animalService;
+    
     @RequestMapping(value="/greeting", method=RequestMethod.GET)
     @ResponseBody
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
-
+    
+    @RequestMapping(value="/animals", method=RequestMethod.GET)
+    @ResponseBody
+    public List<Animal> animals() {
+        return animalService.findAll();
+    }
 }
